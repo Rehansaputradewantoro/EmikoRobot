13e1b71f943f3a23e360b890bc3b4b23d19873e4
Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>
2024-01-21 02:29:07 +0700

    Update events.py

--- EmikoRobot/events.py
+++ EmikoRobot/events.py
@@ -12,7 +12,7 @@ from EmikoRobot import telethn
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
-db = client["emiexrobot"]
+db = client["hanxrobot"]
 gbanned = db.gban
 
 def register(**args):
