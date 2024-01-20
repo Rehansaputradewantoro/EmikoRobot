9c77a6dd801abe7418a42aa7818a297f679f3eee
Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>
2024-01-21 04:55:01 +0700

    Update purge.py

--- EmikoRobot/modules/purge.py
+++ EmikoRobot/modules/purge.py
@@ -21,16 +21,16 @@ async def purge_messages(event):
         )
         and event.from_id not in [1087968824]
     ):
-        await event.reply("Only Admins are allowed to use this command")
+        await event.reply("Perintah Ini Khusus Buat Admin Anjeng")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to purge the message")
+        await event.reply("Lu liat ga? Pesan Gabisa Di Hapus Anjeng! ")
         return
 
     reply_msg = await event.get_reply_message()
     if not reply_msg:
-        await event.reply("Reply to a message to select where to start purging from.")
+        await event.reply("Reply=Balas Pesan Pilih Pesan Yang Mau Di Bersihin Tod! .")
         return
     messages = []
     message_id = reply_msg.id
@@ -48,7 +48,7 @@ async def purge_messages(event):
     except:
         pass
     time_ = time.perf_counter() - start
-    text = f"Purged Successfully in {time_:0.2f} Second(s)"
+    text = f"Pembersihan Kotoran Kucing Secepat {time_:0.2f} Second(s)"
     await event.respond(text, parse_mode="markdown")
 
 async def delete_messages(event):
@@ -62,16 +62,16 @@ async def delete_messages(event):
         )
         and event.from_id not in [1087968824]
     ):
-        await event.reply("Only Admins are allowed to use this command")
+        await event.reply("Lu Bukan Admin Anjeng! ")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to delete this?")
+        await event.reply("Lu Galiat Pesan Gabisa DiHapus?")
         return
 
     message = await event.get_reply_message()
     if not message:
-        await event.reply("Whadya want to delete?")
+        await event.reply("Gua Tanya? Ngapain Lu Nyuruh Hapus Pesan Anjeng! ")
         return
     chat = await event.get_input_chat()
     del_message = [message, event.message]
