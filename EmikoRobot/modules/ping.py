3f0067bb085dafd814590bc761a137c144d16cfe
Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>
2024-01-21 04:59:22 +0700

    Update ping.py

--- EmikoRobot/modules/ping.py
+++ EmikoRobot/modules/ping.py
@@ -79,9 +79,9 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "<b>PONG</b> ✨\n"
-        "<b>Time Taken:</b> <code>{}</code>\n"
-        "<b>Service Uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "<b>PONG</b> 🐽\n"
+        "<b>Masa Aktif Ngentod:</b> <code>{}</code>\n"
+        "<b>Waktu Pembaruan:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 
